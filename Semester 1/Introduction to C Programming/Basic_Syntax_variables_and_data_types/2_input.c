3a0aa3422aceb59c7108befa95e6e7e64ce07075
Md. Asraful Molla <128937137+asrafulmolla@users.noreply.github.com>
2024-04-18 23:49:01 +0600

    Add files via upload

---
+++ Semester 1/Introduction to C Programming/Basic_Syntax_variables_and_data_types/2_input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int rahim;
+    float f;
+    char c;
+    scanf("%d %f %c",&rahim,&f,&c);
+    printf("%d %.2f %c",rahim,f,c);
+    return 0; 
+}
---
+++ Semester 1/Introduction to C Programming/Basic_Syntax_variables_and_data_types/3_variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    // int rahim=100,karim=200;
+    // printf("%d %d",rahim,karim);
+
+    // float rahim=100.567,karim=200.654;
+    // printf("%f %0.2f",rahim,karim);
+
+    char rahim='R',karim='K';
+    printf("%c %c",rahim, karim);
+}
