09e97a7e4309ee28b38747fd7c0566669cd0968f
Md. Asraful Molla <128937137+asrafulmolla@users.noreply.github.com>
2024-04-19 00:27:19 +0600

    Add files via upload

---
+++ Semester 1/Introduction to C Programming/09_Introduction_to_Array/1_Array_decler.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    int a[10]; //10 size er ekti array decler kora hocheche.
+    return 0;
+}
---
+++ Semester 1/Introduction to C Programming/09_Introduction_to_Array/4_array_all_value_zero.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={};
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
