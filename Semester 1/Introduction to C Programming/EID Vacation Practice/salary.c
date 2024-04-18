942e9502bbc922790f03bc6a256d29dd10697b86
Md. Asraful Molla <128937137+asrafulmolla@users.noreply.github.com>
2024-04-19 00:16:46 +0600

    Delete Semester 1/Introduction to C Programming/EID Vacation Practice directory

--- Semester 1/Introduction to C Programming/EID Vacation Practice/salary.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    long long int x;
-    scanf("%lld",&x);
-    printf("%lld",x*365);
-}
--- Semester 1/Introduction to C Programming/EID Vacation Practice/taka.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int x,y;
-    scanf("%d%d",&x,&y);
-    int avg=x-y;
-    int per=avg/2;
-    printf("%d %d",per+y,per);
-}
