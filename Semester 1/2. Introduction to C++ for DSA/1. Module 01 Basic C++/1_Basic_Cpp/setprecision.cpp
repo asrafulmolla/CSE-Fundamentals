c1a1ec1745f5ce72e36e947ebfdeb94707d07bf1
Md. Asraful Molla <128937137+asrafulmolla@users.noreply.github.com>
2024-05-19 03:01:32 +0600

    Delete Semester 1/2. Introduction to C++ for DSA/1. Module 01 Basic C++/1_Basic_Cpp directory

--- Semester 1/2. Introduction to C++ for DSA/1. Module 01 Basic C++/1_Basic_Cpp/setprecision.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<iomanip>
-using namespace std;
-int main()
-{
-    double a;
-    cin>>a;
-    cout<<fixed<<setprecision(2)<<a<<endl;// point pot koto ghor print korbe tar deside kore
-}
